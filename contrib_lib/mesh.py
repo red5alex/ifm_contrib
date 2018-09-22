00a0a7169611fec502d88c2334b15161810e5968
Alexander Renz <alex.renz@gmx.net>
2018-09-22 17:45:44 +0200

    added getCentroid method

--- contrib_lib/mesh.py
+++ contrib_lib/mesh.py
@@ -11,3 +11,38 @@ class Mesh:
         self.doc = doc
 
     # add custom methods here
+
+    def getCentroid(self, item, localcos=False, itemtype=Enum.SEL_ELEMENTAL):
+        """
+        Return the centroid of an element as (X, Y, Z) coordinate Tuple
+        :param item: element number
+        :param localcos: if True, Return local coordinate system (default: global)
+        :param itemtype: {ifm.Enum.SEL_*} default ifm.Enum.SEL_ELEMENTAL
+        :return:
+        """
+        if itemtype != ifm.Enum.SEL_ELEMENTAL:
+            raise NotImplementedError("function not implemented for itemtype " + str(ifm.Enum.SEL_ELEMENTAL))
+
+        dim = self.getNumberOfDimensions()
+        NN = self.doc.getNumberOfNodesPerElement()
+
+        x, y, z = (0., 0., 0.)
+        for N in range(NN):
+            n = self.doc.getNode(item, N)
+            x += self.doc.getX(n)
+            y += self.doc.getY(n)
+            if dim == 3:
+                z += self.doc.getZ(n)
+
+        x = x / NN
+        y = y / NN
+        if dim == 3:
+            z = z / NN
+        else:
+            z = None
+
+        if not localcos:
+            x += self.doc.getOriginX()
+            y += self.doc.getOriginY()
+
+        return x, y, z
