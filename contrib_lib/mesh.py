a40238b1a4d8aeb216be5c43d160b1eb8dd84693
Alexander Renz <alex.renz@gmx.net>
2018-10-30 20:44:15 +0100

    MeshGpd.getGeoDatframe can now export parameter values

--- contrib_lib/mesh.py
+++ contrib_lib/mesh.py
@@ -16,6 +16,62 @@ class Mesh:
 
     # add custom methods here
 
+    def imatrix_as_array(self, global_cos=True, split_quads_to_triangles=False, layer=None, ignore_inactive=False):
+        """
+        load the nodes coordinates, the incidence matrix into a numpy matrix
+        :param global_cos: If True, use global coordinate system (default: local)
+        :return: tuple(numpy.Array) (x, y, imat)
+        """
+
+        import numpy as np
+
+        if global_cos:
+            X0 = self.doc.getOriginX()
+            Y0 = self.doc.getOriginY()
+        else:
+            X0 = Y0 = 0
+
+
+        if self.doc.getNumberOfDimensions() == 2:
+            nn = self.doc.getNumberOfNodesPerSlice()
+            ee = self.doc.getNumberOfElementsPerLayer()
+            stop = 1
+        elif layer is not None:
+            nn = self.doc.getNumberOfNodesPerSlice()
+            ee = self.doc.getNumberOfElementsPerLayer()
+            stop = 2
+        else:
+            nn = self.doc.getNumberOfNodes()
+            ee = self.doc.getNumberOfElements()
+            stop = 1
+
+        x = np.array([self.doc.getX(n) + X0 for n in range(nn)])
+        y = np.array([self.doc.getY(n) + Y0 for n in range(nn)])
+
+        imat = []
+
+        for e in range(ee):  # PerLayer
+
+            if ignore_inactive and not self.doc.getMatElementActive(e):
+                continue
+
+            NN = self.doc.getNumberOfElementNodes(e) / stop
+            element_nodes = [self.doc.getNode(e, N) for N in range(NN)]
+
+
+            if split_quads_to_triangles:
+                if NN == 3:
+                    imat.append(element_nodes)
+                elif NN == 4:
+                    imat.append(element_nodes[:3])  # split quadrangle in 2 triangles
+                    imat.append(element_nodes[1:])
+                else:
+                    raise ValueError(str(NN * 2) + "-noded element not supported")
+            else:
+                imat.append(element_nodes)
+
+        return x, y, imat
+
     def getCentroid(self, item, localcos=False, itemtype=Enum.SEL_ELEMENTAL):
         """
         Return the centroid of an element as (X, Y, Z) coordinate Tuple
--- contrib_lib/mesh_geopandas.py
+++ contrib_lib/mesh_geopandas.py
@@ -7,59 +7,7 @@ class MeshGpd:
         self.doc = doc
 
 
-    def imatrix_as_array(self, global_cos=True):
-        """
-        load the nodes coordinates, the incidence matrix into a numpy matrix
-        :param global_cos: If True, use global coordinate system (default: local)
-        :return: tuple(numpy.Array) (x, y, imat)
-        """
-
-        import numpy as np
-
-        dd = self.doc.getNumberOfDimensions()
-        if global_cos:
-            X0 = self.doc.getOriginX()
-            Y0 = self.doc.getOriginY()
-        else:
-            X0 = Y0 = 0
-
-        x = []
-        y = []
-
-        if dd == 2:
-            nn = self.doc.getNumberOfNodes()
-            ee = self.doc.getNumberOfElements()
-            stop = 1
-        else:
-            nn = self.doc.getNumberOfNodesPerSlice()
-            ee = self.doc.getNumberOfElementsPerLayer()
-            stop = 2
-
-        for n in range(nn):
-            x.append(self.doc.getX(n) + X0)
-            y.append(self.doc.getY(n) + Y0)
-
-        x = np.array(x)
-        y = np.array(y)
-
-        imat = []
-
-        for e in range(ee):  # PerLayer
-            if not self.doc.getMatElementActive(e):
-                continue
-            NN = self.doc.getNumberOfElementNodes(e) / stop
-            element_nodes = [self.doc.getNode(e, N) for N in range(NN)]
-            if NN == 3:
-                imat.append(element_nodes)
-            elif NN == 4:
-                imat.append(element_nodes[:3])  # split quadrangle in 2 triangles
-                imat.append(element_nodes[1:])
-            else:
-                raise ValueError(str(NN * 2) + "-noded element not supported")
-
-        return x, y, imat
-
-    def getGeoDatframe(self, global_cos=True):
+    def getGeoDatframe(self, parameters=None, global_cos=True, ):
         """
         Get the mesh as a GeoPandas GeoDataFrame.
         :param global_cos: If True, use global coordinate system (default: local)
@@ -69,7 +17,7 @@ class MeshGpd:
         import geopandas as gpd
         from shapely.geometry import Point, Polygon
 
-        x, y, imat = self.imatrix_as_array(global_cos=global_cos)
+        x, y, imat = self.doc.c.mesh.imatrix_as_array(global_cos=global_cos, layer=0, split_quads_to_triangles=False)
         # create a GeoDataFrame from the mesh and save as shape file
         gdf_elements = gpd.GeoDataFrame([Polygon([(x[n], y[n]) for n in element]) for element in imat])
         gdf_elements.columns = ["element_shape"]
@@ -77,5 +25,10 @@ class MeshGpd:
         gdf_elements.index.name = "ELEMENT"
         gdf_elements["ELEMENT"] = gdf_elements.index.values
         gdf_elements["AREA"] = gdf_elements.geometry.area
+
+        # export parameters if provided
+        if type(parameters) == dict:
+            for key in parameters:
+                gdf_elements[key] = self.doc.getParamValues(parameters[key])
 
         return gdf_elements
