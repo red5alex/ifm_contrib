66953bada932e15efe7508a09a72c3e248f204c3
Alexander Renz <alex.renz@gmx.net>
2018-10-31 21:26:13 +0100

    added id, node number and modelled head to obspoint dataframe output, renamed function obs.df.getGeoDataframe

--- contrib_lib/obs_geopandas.py
+++ contrib_lib/obs_geopandas.py
@@ -6,7 +6,7 @@ class ObsGpd:
     def __init__(self, doc):
         self.doc = doc
 
-    def getGeoDatframe(self, global_cos=True):
+    def getGeoDataframe(self, global_cos=True):
         """
         Get the observation points as a GeoPandas GeoDataFrame.
         :param global_cos: If True, use global coordinate system (default)
@@ -15,13 +15,14 @@ class ObsGpd:
 
         import geopandas as gpd
         from shapely.geometry import Point, Polygon
+        import numpy as np
 
         # create a list of observation points in FEFLOW model:
         obs = []
-        for o in range(self.doc.getNumberOfValidObsPoints()):
+        for obsid in range(self.doc.getNumberOfValidObsPoints()):
 
             # get geometry
-            x, y, z = self.doc.getXOfObsId(o), self.doc.getYOfObsId(o), self.doc.getZOfObsId(o)
+            x, y, z = self.doc.getXOfObsId(obsid), self.doc.getYOfObsId(obsid), self.doc.getZOfObsId(obsid)
             if global_cos:
                 X0, Y0 = self.doc.getOriginX(), self.doc.getOriginY()
                 x += X0
@@ -30,12 +31,21 @@ class ObsGpd:
             shape = Point(x, y)  # create a geometry object
 
             # get label and append to list
-            label = self.doc.getObsLabel(o)
-            obs.append([label, x, y, z, shape])
+            label = str(self.doc.getObsLabel(obsid))
+
+            # get node number and append to list (None for arbitrary-type observation point)
+            node = self.doc.getTypeOfObsId(obsid)
+            if node < 0:
+                node = np.nan
+
+            # get modelled values
+            h = self.doc.getFlowValueOfObsIdAtCurrentTime(obsid)
+
+            obs.append([obsid, label, x, y, z, node, h, shape])
 
         # create and return dataframe
-        gdf_obs = gpd.GeoDataFrame(obs, columns=["label", "x", "y", "z", "shape"])
-        gdf_obs.set_index("label", inplace=True)
+        gdf_obs = gpd.GeoDataFrame(obs, columns=["id", "label", "x", "y", "z", "node", "h", "shape"])
+        gdf_obs.set_index("id", inplace=True)
         gdf_obs.set_geometry("shape", inplace=True)
 
         return gdf_obs
