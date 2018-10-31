01c3a79f9b53ab0b3a2589078e7df0cbd8335e0b
Alexander Renz <alex.renz@gmx.net>
2018-10-31 21:30:31 +0100

    mesh.getGeodataframe renamed to mesh.df.elements, added layer information, allowed parameter column definition by list.

--- contrib_lib/mesh_geopandas.py
+++ contrib_lib/mesh_geopandas.py
@@ -7,9 +7,10 @@ class MeshGpd:
         self.doc = doc
 
 
-    def getGeoDatframe(self, parameters=None, global_cos=True, ):
+    def elements(self, parameters=None, global_cos=True):
         """
         Get the mesh as a GeoPandas GeoDataFrame.
+        :param parameters: Dict {colname : parid} or List [parid]. Adds values of given parameters as columns.
         :param global_cos: If True, use global coordinate system (default: local)
         :return: GeoDataFrame
         """
@@ -17,18 +18,24 @@ class MeshGpd:
         import geopandas as gpd
         from shapely.geometry import Point, Polygon
 
-        x, y, imat = self.doc.c.mesh.imatrix_as_array(global_cos=global_cos, layer=0, split_quads_to_triangles=False)
+        x, y, imat = self.doc.c.mesh.imatrix_as_array(global_cos=global_cos, layer=None, split_quads_to_triangles=False)
         # create a GeoDataFrame from the mesh and save as shape file
         gdf_elements = gpd.GeoDataFrame([Polygon([(x[n], y[n]) for n in element]) for element in imat])
         gdf_elements.columns = ["element_shape"]
         gdf_elements.set_geometry("element_shape", inplace=True)
         gdf_elements.index.name = "ELEMENT"
         gdf_elements["ELEMENT"] = gdf_elements.index.values
+        gdf_elements["LAYER"] = gdf_elements.index.values / self.doc.getNumberOfElementsPerLayer() + 1
         gdf_elements["AREA"] = gdf_elements.geometry.area
 
         # export parameters if provided
+        if type(parameters) == list:
+            for parameter_id in parameters:
+                gdf_elements[parameter_id] = self.doc.getParamValues(parameter_id)
+
         if type(parameters) == dict:
             for key in parameters:
                 gdf_elements[key] = self.doc.getParamValues(parameters[key])
+
 
         return gdf_elements
