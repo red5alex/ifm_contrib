e01639d55e64f5ed5cf738f07df585a31e717b9d
Alexander Renz <alex.renz@gmx.net>
2018-11-18 23:11:09 +0100

    rewritten plot_geopandas.py (currently duplicates code with plot.py)

--- contrib_lib/plot_geopandas.py
+++ contrib_lib/plot_geopandas.py
@@ -35,7 +35,40 @@ class PlotGpd:
 
         return values
 
-    def fringes(self, item, subitem=0, global_cos=True):
+    def _tricontourset_to_gdf(self, tricontourset, itemname):
+        """
+        Create a GeoPandas.GeoDataFrame from a TriContourSet object
+        :return: geodataframe
+        """
+
+        import geopandas as gpd
+        from shapely.geometry import Polygon, MultiPolygon
+
+        # create list of Multipolygons from contourset
+        fringes_multipolygons = []
+        for collection in tricontourset.collections:
+            polygons = []
+            assert len(collection.get_paths()) == 1, "more than one path found"
+            for path in collection.get_paths():
+                for polygon in path.to_polygons():
+                    polygons.append(Polygon(polygon))
+            fringes_multipolygons.append(MultiPolygon(polygons))
+
+        # create new dataframe
+        gdf = gpd.GeoDataFrame()
+
+        # use fringes as geometry
+        gdf["geometry"] = fringes_multipolygons
+        gdf.set_geometry("geometry", inplace=True)
+
+        # add limits and layers to gdf
+        gdf["layer"] = tricontourset.layers
+        gdf[str(itemname) + "_min"] = tricontourset.levels[:-1]
+        gdf[str(itemname) + "_max"] = tricontourset.levels[1:]
+
+        return gdf
+
+    def fringes(self, item=None, expr=None, distr=None, slice=1, global_cos=True, levels=None, species=None):
         """
         Get Fringes Polygons.
         :param global_cos: If True, use global coordinate system (default: local)
@@ -48,29 +81,66 @@ class PlotGpd:
         import matplotlib.pyplot as plt
         import matplotlib.tri as tri
 
-        # read incidence matrix and node values
-        x, y, imat = self.doc.c.mesh.gdf.imatrix_as_array(global_cos=global_cos)
+        # set current species
+        if species is not None:
+            if type(species) == str:
+                speciesID = self.doc.findSpecies(species)
+            elif type(species) == int:
+                speciesID = species
+            else:
+                raise ValueError("species must be string (for name) or integer (for id)")
+            self.doc.setMultiSpeciesId(speciesID)
+
+        # read incidence matrix and node coordinates
+        imat = self.doc.c.mesh.get_imatrix2d(slice=slice, ignore_inactive=True, split_quads_to_triangles=True)
+        x, y = self.doc.getParamValues(Enum.P_MSH_X), self.doc.getParamValues(Enum.P_MSH_Y)
+
+        if global_cos:
+            X0 = self.doc.getOriginX()
+            Y0 = self.doc.getOriginY()
+            x = [X + X0 for X in x]
+            y = [Y + Y0 for Y in y]
+
+        # create Triangulation object
         femesh = tri.Triangulation(x, y, np.asarray(imat))
 
-        # get values and generate polygons from matplotlib (suppress output)
-        values = self._get_nodal_values(item)
-        fig, ax = plt.subplots(1, 1)
-        fringes = ax.tricontourf(femesh, values, cmap='rainbow')
+        # get values, remove nan values (=inactive elements)
+        if item is not None:
+            values = self.doc.getParamValues(item)
+        elif expr is not None:
+            if type(expr) == str:
+                exprID = self.doc.getNodalExprDistrIdByName(expr)
+            elif type(expr) == int:
+                exprID = expr
+            else:
+                raise ValueError("expr must be string (for name) or integer (for id)")
+            values = [self.doc.getNodalExprDistrValue(exprID, n) for n in range(self.doc.getNumberOfNodes())]
+        elif distr is not None:
+            if type(distr) == str:
+                distrID = self.doc.getNodalRefDistrIdByName(distr)
+            elif type(distr) == int:
+                distrID = distr
+            else:
+                raise ValueError("distr must be string (for name) or integer (for id)")
+            values = [self.doc.getNodalRefDistrValue(distrID, n) for n in range(self.doc.getNumberOfNodes())]
+        else:
+            raise ValueError("either of parameter item, expr or distr must be provided!")
+
+        # set nan values to zero
+        values = np.nan_to_num(np.asarray(values))
+
+        # generate polygons from matplotlib (suppress output)
+        fig, ax = plt.subplots(1, 1, figsize=(0,0))
+        if levels is None:
+            fringes = ax.tricontourf(femesh, values,)
+        else:
+            fringes = ax.tricontourf(femesh, values, levels)
         _ = ax.remove()
 
-        # create geodataframe from fringes
-        p = []
-        for collection in fringes.collections:
-            for path in collection.get_paths():
-                for polygon in path.to_polygons():
-                    p.append(Polygon(polygon))
-        gdf = gpd.GeoDataFrame(p)
-        gdf.columns = ["geometry"]
-        gdf.set_geometry("geometry", inplace=True)
-        gdf[str(item)+"_min"] = fringes.levels[:-1]
-        gdf[str(item)+"_max"] = fringes.levels[1:]
 
-        return gdf
+        return self._tricontourset_to_gdf(fringes, itemname=item)
+
+
 
     def isolines(self, item, subitem=0, global_cos=True):
         """
