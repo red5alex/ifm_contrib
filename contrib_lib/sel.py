207874ca738e9f789a9f1d7efd0b92ec42fc0416
Alexander Renz <alex.renz@gmx.net>
2018-12-19 08:35:09 +0100

    added sel.covert() and support functions

--- contrib_lib/sel.py
+++ contrib_lib/sel.py
@@ -47,3 +47,51 @@ class Sel:
         else:
             nsel = self.doc.getNumberOfSelections(seltype)
             return [self.doc.getSelectionName(seltype, selid) for selid in range(nsel)]
+
+    def getSelectionType(self, selection):
+        """
+        Returns the type of the selection. Returns -1 (Enum.SEL_INVALID) if selection does not exist.
+        :param selection: name of the selection to be tested
+        :return: type of selection
+        """
+        for seltype in [Enum.SEL_NODAL,
+                        Enum.SEL_ELEMENTAL,
+                        Enum.SEL_EDGES,
+                        # Enum.SEL_FACES,  ARE: inactive, probably FEFLOW bug
+                        Enum.SEL_FRACS]:
+
+            selections = self.doc.c.sel.getSelectionNames(seltype)
+            if selection in selections:
+                return (seltype)
+        return Enum.SEL_INVALID
+
+    def convert(self, selection, to_type):
+        """
+        Converts a selection to a selection of the given type
+        :param selection:
+        :param to_type:
+        :return:
+        """
+        from_type = self.doc.c.sel.getSelectionType(selection)
+
+        if from_type==Enum.SEL_INVALID:
+            raise ValueError("Selection {} not found".format(selection))
+
+        if to_type==Enum.SEL_INVALID:
+            raise ValueError("Cannot convert to Invalid Type!")
+
+        # no conversion if types are equal
+        if from_type==to_type:
+            return self.doc.c.sel.list(selection)
+
+        # elemental to nodal
+        if from_type == Enum.SEL_ELEMENTAL and to_type == Enum.SEL_NODAL:
+            i_matrix = self.doc.c.mesh.get_imatrix()
+            to_sel = set()
+            for e in self.doc.c.sel.list(selection):
+                to_sel = to_sel.union(set(i_matrix[e]))
+
+            return sorted(list(to_sel))
+
+        # all other cases not implemented
+        raise NotImplementedError()
---
+++ unittests/test_sel.py
@@ -0,0 +1,70 @@
+import unittest
+import ifm_contrib as ifm
+from ifm import Enum
+
+
+class TestSel(unittest.TestCase):
+
+    def test_sel_convert_2D(self):
+        ifm.forceLicense("Viewer")
+        self.doc = ifm.loadDocument(r".\models\example_2D.fem")
+
+        self.assertEqual(self.doc.c.sel.getSelectionType("conversiontest_el"), 1)
+        self.assertEqual(self.doc.c.sel.getSelectionType("conversiontest_doesnotexist"), -1)
+
+        # -> invalid is not possible
+        self.assertRaises(ValueError,
+                          self.doc.c.sel.convert,
+                          "conversiontest_el", Enum.SEL_INVALID)
+
+        # no conversion on equal types
+        self.assertEqual(self.doc.c.sel.convert("conversiontest_el", Enum.SEL_ELEMENTAL),
+                         [586, 955])
+        self.assertEqual(self.doc.c.sel.convert("conversiontest_el", Enum.SEL_ELEMS),
+                         [586, 955])
+
+        # elemental -> nodal
+        self.assertEqual(self.doc.c.sel.convert("conversiontest_el", Enum.SEL_NODAL),
+                         [369, 370, 465, 467, 480])
+        self.assertEqual(self.doc.c.sel.convert("conversiontest_el", Enum.SEL_NODES),
+                         [369, 370, 465, 467, 480])
+
+        # other not yet implemented
+        with self.assertRaises(NotImplementedError):
+            self.doc.c.sel.convert("conversiontest_el", Enum.SEL_FACES)
+            self.doc.c.sel.convert("conversiontest_el", Enum.SEL_FRACS)
+            self.doc.c.sel.convert("conversiontest_el", Enum.SEL_EDGES)
+
+        self.doc.closeDocument()
+
+    def test_sel_convert_3D(self):
+        ifm.forceLicense("Viewer")
+        self.doc = ifm.loadDocument(r".\models\example_3D_mspecies.fem")
+
+        self.assertEqual(self.doc.c.sel.getSelectionType("conversiontest_el"), 1)
+        self.assertEqual(self.doc.c.sel.getSelectionType("conversiontest_doesnotexist"), -1)
+
+        # -> invalid is not possible
+        self.assertRaises(ValueError,
+                          self.doc.c.sel.convert,
+                          "conversiontest_el", Enum.SEL_INVALID)
+
+        # no conversion on equal types
+        self.assertEqual(self.doc.c.sel.convert("conversiontest_el", Enum.SEL_ELEMENTAL),
+                         [718, 971])
+        self.assertEqual(self.doc.c.sel.convert("conversiontest_el", Enum.SEL_ELEMS),
+                         [718, 971])
+
+        # elemental -> nodal
+        self.assertEqual(self.doc.c.sel.convert("conversiontest_el", Enum.SEL_NODAL),
+                         [455, 477, 478, 510, 523, 1040, 1062, 1063, 1095, 1108])
+        self.assertEqual(self.doc.c.sel.convert("conversiontest_el", Enum.SEL_NODES),
+                         [455, 477, 478, 510, 523, 1040, 1062, 1063, 1095, 1108])
+
+        # other not yet implemented
+        with self.assertRaises(NotImplementedError):
+            self.doc.c.sel.convert("conversiontest_el", Enum.SEL_FACES)
+            self.doc.c.sel.convert("conversiontest_el", Enum.SEL_FRACS)
+            self.doc.c.sel.convert("conversiontest_el", Enum.SEL_EDGES)
+
+        self.doc.closeDocument()
