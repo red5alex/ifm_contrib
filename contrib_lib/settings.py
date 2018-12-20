ddacc92962b7804f4c6884a69507272762322895
Alexander Renz <alex.renz@gmx.net>
2018-12-20 21:17:59 +0100

    corrected doc-string syntax

--- contrib_lib/settings.py
+++ contrib_lib/settings.py
@@ -5,8 +5,8 @@ class Settings:
     """
     Extension child-class for IFM contributor's Extensions.
     Use this class to add functionality relating to PROBLEM SETTINGS.
-    Note: Functionality relating diagnosing problems in the model should be implented in the
-          .diag class of this project.
+    Note: Functionality relating diagnosing problems in the model should be implented in the .diag class of this
+    project.
     """
 
     def __init__(self, doc):
