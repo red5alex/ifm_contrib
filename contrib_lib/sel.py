807f47d338742f477a56a4b473cd80f934b1a985
Alexander Renz <alex.renz@gmx.net>
2018-12-20 21:17:07 +0100

    added an option to create an item set directly from an item list.

--- contrib_lib/sel.py
+++ contrib_lib/sel.py
@@ -11,7 +11,15 @@ class Sel:
         self.doc = doc
 
     # add custom methods here
+
     def list(self, selname, seltype=None):
+        """
+        Return the item indices of the given selection as a list.
+        :param selname: name of the selection
+        :param seltype: type of the selection (optional)
+        :return:
+        """
+
         # try all supported types
         if seltype is None:
             seltypes = [Enum.SEL_NODES,
@@ -30,6 +38,12 @@ class Sel:
         return []
 
     def set(self, selname, seltype=None):
+        """
+        Return the item indices of the given selection as a set.
+        :param selname: name of the selection
+        :param seltype: type of the selection (optional)
+        :return:
+        """
         return set(self.list(selname, seltype=seltype))
 
     def getSelectionNames(self, seltype=None):
@@ -62,8 +76,30 @@ class Sel:
 
             selections = self.doc.c.sel.getSelectionNames(seltype)
             if selection in selections:
-                return (seltype)
+                return seltype
         return Enum.SEL_INVALID
+
+    def create(self, seltype, selname, itemlist=None):
+        """
+        create a new selection of given type and name. Populate the selection if itemlist is provided.
+        :param seltype: Type of selection type (see ifm.Enum.SEL_<>)
+        :param selname: Name of selection
+        :param itemlist: list of item indices (optional)
+        :return: the id of the selection
+        """
+
+        # raise error if selection already exists
+        if not self.doc.findSelection(seltype, selname) == -1:
+            raise ValueError("Selection {} does already exist!".format(selname))
+
+        selid = self.doc.createSelection(seltype, selname)
+
+        # populate if itemlist is provided
+        if itemlist is not None:
+            for i in itemlist:
+                self.doc.setSelectionItem(seltype, selid, i)
+
+        return selid
 
     def convert(self, selection, to_type):
         """
@@ -74,14 +110,14 @@ class Sel:
         """
         from_type = self.doc.c.sel.getSelectionType(selection)
 
-        if from_type==Enum.SEL_INVALID:
+        if from_type == Enum.SEL_INVALID:
             raise ValueError("Selection {} not found".format(selection))
 
-        if to_type==Enum.SEL_INVALID:
+        if to_type == Enum.SEL_INVALID:
             raise ValueError("Cannot convert to Invalid Type!")
 
         # no conversion if types are equal
-        if from_type==to_type:
+        if from_type == to_type:
             return self.doc.c.sel.list(selection)
 
         # elemental to nodal
