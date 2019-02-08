005968a4fef1125665a447a63c7086dc415a33ed
Alexander Renz <alex.renz@gmx.net>
2019-02-08 10:10:57 +0100

    added functions for time series (power function) handling

--- contrib_lib/ts.py
+++ contrib_lib/ts.py
@@ -1,4 +1,5 @@
-from ifm import Enum
+# from ifm import Enum
+from .ts_pandas import TsPd
 
 
 class Ts:
@@ -11,4 +12,63 @@ class Ts:
     def __init__(self, doc):
         self.doc = doc
 
+        # add custom child-classes here
+        self.df = TsPd(doc)
+
     # add custom methods here
+    def info(self):
+        """
+        Returns information on existing time series (formerly power functions) as a list of tuples:
+        [(Time Series ID, Comment, Number of Points, cyclic, interpolation kind)]
+        """
+        info = []
+        tsid = 0
+        while True:
+            tsid = self.doc.powerGetCurve(tsid)
+            if tsid == -1:
+                break
+            info.append((tsid,
+                         self.doc.powerGetComment(tsid),
+                         self.doc.powerGetNumberOfPoints(1),
+                         self.doc.powerIsCyclic(1),
+                         self.doc.powerGetInterpolationKind(1)))
+        return info
+
+    def points(self, tsid):
+        """
+        Returns the values of a given time series as a list of (time, value) tuples.
+
+        :param tsid:             time series ID
+        :type tsid:              int or convertible to int
+        :return:                 list of tuples
+        """
+
+        # make sure tsid is a valid number
+        try:
+            tsid = int(tsid)
+        except ValueError:
+            raise ValueError("tsid must be of type int or convertible to type int")
+
+        return [self.doc.powerGetPoint(tsid, i) for i in range(self.doc.powerGetNumberOfPoints(tsid))]
+
+    def exists(self, tsid):
+        """
+        Test if time series (formerly power function) exists.
+
+        :param tsid:  time series ID
+        :type tsid:   int or convertible to int
+        :return:
+        """
+
+        # make sure tsid is a valid number
+        try:
+            tsid = int(tsid)
+        except ValueError:
+            raise ValueError("tsid must be of type int or convertible to type int")
+
+        # check if ts is in list of tsids
+        tsid_existing = [i[0] for i in self.doc.c.ts.info()]
+        if tsid in tsid_existing:
+            return True
+        else:
+            return False
---
+++ contrib_lib/ts_pandas.py
@@ -0,0 +1,67 @@
+# from ifm import Enum
+import pandas as pd
+
+
+class TsPd:
+
+    def __init__(self, doc):
+        self.doc = doc
+
+    def info(self):
+        """
+        Returns a pandas.DataFrame with information on existing time series (formerly power functions).
+        """
+
+        list_info = self.doc.c.ts.info()
+        df = pd.DataFrame(list_info, columns=["tsid", "comment", "no_point", "is_cyclic_num", "interpolation_kind"])
+        df.set_index("tsid", inplace=True)
+        df["is_cyclic"] = df.is_cyclic_num.astype(bool)
+        del (df["is_cyclic_num"])
+        return df
+
+    def points(self, tsid, force_time_axis=False, reference_time=None):
+        """
+        Returns the values of a given time series (formerly power function) as a dataframe.
+
+        :param tsid:             time series ID
+        :type tsid:              int or convertible to int
+        :param force_time_axis:  If True, the index of the dataframe will be the simulation time in days.
+                                 If False (default), the index type will be of type datetime if a reference time is set
+                                 in the model, and simulation time in days otherwise.
+        :type force_time_axis:   bool
+        :param reference_time:   Specify (or override) a reference time. Note that this only accounts for this export
+                                 and is not a permanent change of the model settings.
+        :type reference_time:    datetime.datetime
+
+        :return:                 time series as a pandas.DataFrame
+        """
+
+        # make sure tsid is a valid number
+        try:
+            tsid = int(tsid)
+        except ValueError:
+            raise ValueError("tsid must be of type int or convertible to type int")
+
+        # test if time series exists
+        if not self.doc.c.ts.exists(tsid):
+            raise ValueError("Time Series {} does not exist.".format(tsid))
+
+        # get list of points and convert to DataFrame
+        df = pd.DataFrame(self.doc.c.ts.points(tsid), columns=["Simulation Time", "Values"])
+        df.set_index("Simulation Time", inplace=True)
+
+        # convert time axis to datetime
+        if self.doc.getReferenceTime() is None and reference_time is None:
+            force_time_axis = True
+
+        if reference_time is None:
+            reference_time = self.doc.getReferenceTime()
+
+        if force_time_axis:
+            # we are done here, return df
+            return df
+
+        # convert time axis to datetime
+        df["Simulation Time"] = pd.to_datetime(df.index, unit="D", origin=reference_time)
+        df.set_index("Simulation Time", inplace=True)
+        return df
