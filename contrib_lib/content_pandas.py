3873ea431e1b9e670bc3920461d01625dbdb1b70
Alexander Renz <alex.renz@gmx.net>
2019-10-22 18:12:56 +0200

    further incomplete work on doc.c.content

--- contrib_lib/content_pandas.py
+++ contrib_lib/content_pandas.py
@@ -29,24 +29,52 @@ class ContentPd:
         df_info.columns = ["id", "ifm.Enum", "description", "unit"]
         return df_info.set_index("id")
 
-    def content(self):
+
+    def content(self, model_domain=True, selection=None, content_types=True):
         """
         Get the model domains content (all content items).
 
         :return: DataFrame with all contents
         :rtype: pandas.DataFrame
         """
+
+        # if single selection, make list with single entry
+        if type(selection) != list:
+            selection = [selection]
+
+        # raise error if selection list entries are invalid
+        for s in selection:
+            if s is not None:
+                if type(s) != str:
+                    raise ValueError("selection must be None or str, or a list containing str and/or None members")
+                if self.doc.findSelection == -1:
+                    raise ValueError("No elemental selection '{}' found.".format(s))
+
+        # get content by element
+        df_elemental_content = self.doc.c.sel.df.elemental_content(content_types)
+
+        for content_type in content_types:
+            for sel in selection:
+                if sel is None:
+                    content = df_elemental_content[content_type].sum()
+                else:
+                    content = df_elemental_content.loc[self.doc.c.sel.list(sel)].TOTAL_VOLUME.sum()
+
+        # TODO: the 'selection' parameter shall have this behavior:
+        #       None - do nothing
+        #       str - calculate the content of the elemental selection with this name
+        #       [str, str] - calculate the content for multiple selections with this these names
+
         import numpy as np
 
         df_content = self.doc.c.content.df.info()
         df_content["content"] = 0.
         for i in df_content.index:
-            print(i)
             for e in range(self.doc.getNumberOfElements()):
                 try:
-                    df_content.loc[i, "content"] += self.doc.getElementalContent(i, e)
+                    df_content.loc[i, "Model Domain"] += self.doc.getElementalContent(i, e)
                 except StandardError as e:
-                    df_content.loc[i, "content"] = np.nan
+                    df_content.loc[i, "Model Domain"] = np.nan
                     break
 
         return df_content
