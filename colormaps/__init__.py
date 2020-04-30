66559a46b900c80a2f0a774f1fb3ecad60a1724f
Alexander Renz <alex.renz@gmx.net>
2020-04-30 22:53:46 +0200

    added function for showing all colorscales

--- colormaps/__init__.py
+++ colormaps/__init__.py
@@ -8,6 +8,7 @@ If anybody has some time, feel free to get going.
 
 import glob, os
 import xml.etree.ElementTree as ET
+import ifm_contrib as ifm
 
 try:
     from matplotlib.colors import LinearSegmentedColormap
@@ -59,3 +60,43 @@ for filename in glob.glob(local_directory + "/*.xml"):
     cmap = create_colormap_from_xml(cmapname, filename)
     cmaps_map[cmapname] = cmap
 cmaps = list(cmaps_map.keys())
+
+
+def plot_feflow_colorgradients():
+    """
+    This code was adapted from
+    https://matplotlib.org/examples/color/colormaps_reference.html
+    """
+
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    cmaps = [('FEFLOW ',
+                ifm.colormaps.cmaps),]
+
+    nrows = max(len(cmap_list) for cmap_category, cmap_list in cmaps)
+    gradient = np.linspace(0, 1, 256)
+    gradient = np.vstack((gradient, gradient))
+
+
+    def plot_color_gradients(cmap_category, cmap_list, nrows):
+        fig, axes = plt.subplots(nrows=nrows)
+        fig.subplots_adjust(top=0.95, bottom=0.01, left=0.2, right=0.99)
+        axes[0].set_title(cmap_category + ' colormaps', fontsize=14)
+
+        for ax, name in zip(axes, cmap_list):
+            ax.imshow(gradient, aspect='auto', cmap=plt.get_cmap(name))
+            pos = list(ax.get_position().bounds)
+            x_text = pos[0] - 0.01
+            y_text = pos[1] + pos[3]/2.
+            fig.text(x_text, y_text, name, va='center', ha='right', fontsize=10)
+
+        # Turn off *all* ticks & spines, not just the ones with colormaps.
+        for ax in axes:
+            ax.set_axis_off()
+
+
+    for cmap_category, cmap_list in cmaps:
+        plot_color_gradients(cmap_category, cmap_list, nrows)
+
+    plt.show()
