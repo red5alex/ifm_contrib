c693f8aacc8875ddfdf624b68cb68f8a10a71567
Alexander Renz <alex.renz@gmx.net>
2020-06-16 12:24:09 +0200

    bugfix: crashed when having no selections

--- contrib_lib/user_pandas.py
+++ contrib_lib/user_pandas.py
@@ -13,24 +13,28 @@ class UserPd:
         """
 
         # elemental Distribution
-        df_dist_e = pd.DataFrame(
-            [self.doc.getElementalRefDistrName(i) for i in range(self.doc.pdoc.getNumberOfElementalRefDistr())])
-        df_dist_e.columns = ["Name"]
-        df_dist_e["user_type"] = "DISTRIBUTION"
-        df_dist_e["item_type"] = "ELEMENTAL"
-        df_dist_e.index.name = "ID"
-        df_dist_e.reset_index(inplace=True)
+        if self.doc.pdoc.getNumberOfElementalRefDistr() > 0:
+            df_dist_e = pd.DataFrame(
+                [self.doc.getElementalRefDistrName(i) for i in range(self.doc.pdoc.getNumberOfElementalRefDistr())])
+            df_dist_e.columns = ["Name"]
+            df_dist_e["user_type"] = "DISTRIBUTION"
+            df_dist_e["item_type"] = "ELEMENTAL"
+            df_dist_e.index.name = "ID"
+            df_dist_e.reset_index(inplace=True)
+        else:
+            df_dist_e = pd.DataFrame(columns=["Name", "user_type", "item_type"])
 
-        # elemental Distribution
-        df_dist_n = pd.DataFrame(
+        # nodal Distribution
+        if self.doc.pdoc.getNumberOfNodalRefDistr() > 0:
+            df_dist_n = pd.DataFrame(
             [self.doc.getNodalRefDistrName(i) for i in range(self.doc.pdoc.getNumberOfNodalRefDistr())])
-        df_dist_n.columns = ["Name"]
-        df_dist_n["user_type"] = "DISTRIBUTION"
-        df_dist_n["item_type"] = "NODAL"
-        df_dist_n.index.name = "ID"
-        df_dist_n.reset_index(inplace=True)
-
-        # TODO: Does not work yet!
+            df_dist_n.columns = ["Name"]
+            df_dist_n["user_type"] = "DISTRIBUTION"
+            df_dist_n["item_type"] = "NODAL"
+            df_dist_n.index.name = "ID"
+            df_dist_n.reset_index(inplace=True)
+        else:
+            df_dist_n = pd.DataFrame(columns=["Name", "user_type", "item_type"])
 
         # elemental Expression
         if self.doc.pdoc.getNumberOfElementalExprDistr() > 0:
@@ -45,12 +49,15 @@ class UserPd:
             df_expr_e = pd.DataFrame(columns=["Name", "user_type", "item_type"])
 
         # nodal Expression
-        df_expr_n = pd.DataFrame(
+        if self.doc.pdoc.getNumberOfNodalExprDistr() > 0:
+            df_expr_n = pd.DataFrame(
             [self.doc.getNodalExprDistrName(i) for i in range(self.doc.pdoc.getNumberOfNodalExprDistr())])
-        df_expr_n.columns = ["Name"]
-        df_expr_n["user_type"] = "EXPRESSION"
-        df_expr_n["item_type"] = "NODAL"
-        df_expr_n.index.name = "ID"
-        df_expr_n.reset_index(inplace=True)
+            df_expr_n.columns = ["Name"]
+            df_expr_n["user_type"] = "EXPRESSION"
+            df_expr_n["item_type"] = "NODAL"
+            df_expr_n.index.name = "ID"
+            df_expr_n.reset_index(inplace=True)
+        else:
+            df_expr_n = pd.DataFrame(columns=["Name", "user_type", "item_type"])
 
         return pd.concat([df_dist_e, df_dist_n, df_expr_e, df_expr_n])
