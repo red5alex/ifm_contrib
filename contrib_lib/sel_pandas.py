b9691f8c944c878db08803475103878fe145cfd9
Alexander Renz <alex.renz@gmx.net>
2020-07-17 23:40:10 +0200

    added nodal budgeting to doc.c.sel.df.selections()

--- contrib_lib/sel_pandas.py
+++ contrib_lib/sel_pandas.py
@@ -9,17 +9,19 @@ class SelPd:
     def __init__(self, doc):
         self.doc = doc
 
-    def selections(self, seltype=None):
+    def selections(self, seltype=None, budget=False):
         """
         Returns a DataFrame with information on selections of the selected type.
+
         :param seltype: One of ifm.Enum.SEL_*
         :return: DataFrame with information on selections
         :rtype: pandas.DataFrame
         """
 
         import pandas as pd
+        import numpy as np
 
-        # check parameters
+         # check parameters
         supported_types = [Enum.SEL_NODES,
                             Enum.SEL_ELEMS,
                             Enum.SEL_EDGES,
@@ -50,5 +52,22 @@ class SelPd:
             df["N_ITEMS"] = [self.doc.getSelectionItemCount(seltype, i) for i, row in df.iterrows()]
             df.set_index("selection_name", inplace=True)
             df_all = pd.concat([df_all, df])
+
+        if budget==True:
+            # create budget columns, initialize with nan (for non-nodal selections)
+            df_all['budget_flow_bc'] = np.nan
+            df_all['budget_flow_area'] = np.nan
+            df_all['budget_flow_storage'] = np.nan
+
+            # compute nodal budget
+            df_nodes = self.doc.c.mesh.df.nodes(budget=True)
+
+            # aggregate nodal flow values to nodal selections (for each of the 3 budget types)
+            node_selections = self.doc.c.sel.getSelectionNames(seltype=Enum.SEL_NODES)
+            for b in ['budget_flow_bc', 'budget_flow_area', 'budget_flow_storage']:
+                ds_budget = pd.Series(
+                    data=[df_nodes.loc[df_nodes.index.isin(self.doc.c.sel.list(selname))].sum()[b] for selname in node_selections],
+                    index=node_selections)
+                df_all[b] = ds_budget
 
         return df_all
