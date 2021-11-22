798052438fb85bb591f775ae240bd6cb87fcb56d
Alexander Renz <alex.renz@gmx.net>
2021-11-22 15:30:56 +0100

    added more functions to ifm.c.Particle and ifm.c.ParticleSwarm

--- contrib_lib/particles.py
+++ contrib_lib/particles.py
@@ -22,9 +22,10 @@ class ParticleTracer:
         self.ParticleSwarm = ParticleSwarm
 
     def particle(self, seed_point, time_start=None,
-                 settings=None,
+                 forward=False, unsteady=False, random_walk=False, settings=None,
                  global_cos=True, geodataframe=True):
         """
+        Creates a single particle object
 
         :param seed_point:
         :param time_start:
@@ -35,49 +36,19 @@ class ParticleTracer:
         """
 
         return Particle(self.doc, seed_point=seed_point, time_start=time_start,
-                        settings=settings,
+                        forward=forward, unsteady=unsteady, random_walk=random_walk, settings=settings,
                         global_cos=global_cos, geodataframe=geodataframe)
 
-    def particle_swarm(self, default_settings=None):
+    def particle_swarm(self, forward=False, unsteady=False, random_walk=False,  default_settings=None):
         """
-
+        Creates a ParticleSwarm object
 
         :param default_settings:
         :return:
         """
-        return ParticleSwarm(self.doc, default_settings=default_settings)
+        return ParticleSwarm(self.doc, forward=forward, unsteady=unsteady, random_walk=random_walk,
+                             default_settings=default_settings)
 
-    # add custom methods here
-
-    '''
-    def list(self, selname, seltype=None):
-        """
-        Return the item indices of the given selection as a list.
-
-        :param selname: name of the selection
-        :type selname:  str
-        :param seltype: type of the selection (optional)
-        :type seltype:  ifm.Enum or None
-        :return:        list of item indices
-        """
-
-        # try all supported types
-        if seltype is None:
-            seltypes = [Enum.SEL_NODES,
-                        Enum.SEL_ELEMS,
-                        Enum.SEL_EDGES,
-                        Enum.SEL_FRACS]
-        else:
-            seltypes = [seltype]
-
-        for stype in seltypes:
-            selid = self.doc.findSelection(stype, selname)
-            if selid != -1:  # if selection is found
-                return self.doc.getSelectionItems(stype, selid)
-
-        # if selection is not found
-        raise ValueError("Selection {} not found".format(selname))
-    '''
 
 
 class Particle:
@@ -89,27 +60,36 @@ class Particle:
     """
 
     def __init__(self, doc, seed_point, time_start=None,
-                 settings=None,
-                 global_cos=True, geodataframe=True):
+                 forward=False, unsteady=False, random_walk=False, settings=None,
+                 global_cos=True,
+                 geodataframe=True):
         """
         Creates a particle from its seed_point and -time.
         """
-
-        # TODO: support for shapely geometry as seed_point
-        # TODO: add plot function
-        # TODO: add starttime
 
         # PARAMETER HANDLING
 
         self.doc = doc
 
-        self.seed_point = seed_point
-        self.seed_x = seed_point[0]
-        self.seed_y = seed_point[1]
-        if len(seed_point) == 3:
-            self.seed_z = seed_point[2]
+        if type(seed_point) == Point:
+            self.seed_point = seed_point
+            self.seed_x = seed_point.x
+            self.seed_y = seed_point.y
+            if seed_point.has_z:
+                self.seed_z = seed_point.z
+            else:
+                self.seed_z = None
+        elif type(seed_point) == tuple:
+            self.seed_x = seed_point[0]
+            self.seed_y = seed_point[1]
+            if len(seed_point) == 3:
+                self.seed_z = seed_point[2]
+                self.seed_point = Point(self.seed_x, self.seed_y, self.seed_z)
+            else:
+                self.seed_z = None
+                self.seed_point = Point(self.seed_x, self.seed_y)
         else:
-            self.seed_z = None
+            ValueError(f"seed_point of type {type(seed_point)} not supported")
 
         self.time_start = time_start
 
@@ -127,6 +107,24 @@ class Particle:
         if settings is None:
             settings = {}
 
+        if "tracking_direction" not in settings.keys():
+            if forward:
+                settings["tracking_direction"] = Enum.PT_DIR_FORWARD
+            else:
+                settings["tracking_direction"] = Enum.PT_DIR_BACKWARD
+
+        if "tracking_mode" not in settings.keys():
+            if unsteady:
+                settings["tracking_mode"] = Enum.PT_MODE_UNSTEADY
+            else:
+                settings["tracking_mode"] = Enum.PT_MODE_STEADY
+
+        if "tracking_type" not in settings.keys():
+            if random_walk:
+                settings["tracking_type"] = Enum.PT_TYPE_RW
+            else:
+                settings["tracking_type"] = Enum.PT_TYPE_ADVECTIVE
+
         if "tracking_mode" in settings.keys():
             self.pt.setTrackingMode(settings["tracking_mode"])
         if "tracking_direction" in settings.keys():
@@ -134,20 +132,24 @@ class Particle:
         if "tracking_type" in settings.keys():
             self.pt.setTrackingType(settings["tracking_type"])
 
-            # set start time
+        # set start time
         if time_start is not None:
             # convert to days if datetime is provided
-            if type(time_start) is datetime:
+
+            if type(time_start) is datetime or pd.Timestamp:
                 time_start = doc.c.sim.calendar_to_simtime(time_start)
 
             # set start time
             self.pt.setInitialTime(time_start)
 
-            if self.pt.getTrackingMode() == Enum.PT_MODE_STEADY:
+            if self.pt.getTrackingMode() == Enum.PT_MODE_STEADY and doc.getTimeClass() != Enum.TCLS_STEADY:
                 doc.c.sim.load_first_ts_after(time_start)
 
         # generate pathline and load into DataFrame
-        self.pl = self.pt.generatePathLine(self.seed_x, self.seed_y, self.seed_z)
+        if self.seed_z is None:
+            self.pl = self.pt.generatePathLine(self.seed_x, self.seed_y)
+        else:
+            self.pl = self.pt.generatePathLine(self.seed_x, self.seed_y, self.seed_z)
 
         if self.doc.getNumberOfDimensions() == 2:
             self.df = pd.DataFrame(self.pl, columns=["x", "y", "tau"])
@@ -161,9 +163,11 @@ class Particle:
             self.df.y = self.df.y + self.doc.getOriginY()
 
         # add sim_time and calendar axis
-        self.df["sim_time"] = self.df.tau + self.doc.getAbsoluteSimulationTime()
+        self.df["sim_time"] = self.df.tau + time_start
         if self.doc.getReferenceTime() is not None:
             self.df["calendar"] = self.doc.getReferenceTime() + self.df.sim_time.apply(timedelta)
+
+
 
             # create geometric objects
         if geodataframe:
@@ -208,23 +212,32 @@ class Particle:
             return df
 
         # recursive call
-        elif type(time) == datetime:
+        elif type(time) == datetime or pd.Timestamp:
             # raise error if there is no defined reference time
             if self.doc.getReferenceTime() is None:
                 raise RuntimeError("Reference Time is not set in model.")
 
             # if the exact time does already exist, simply return the row
             if np.datetime64(time) in self.df.calendar.values:
-                return self.df.set_index("calendar").loc[time]
-
-            # add the time point into the index, then interpolate over the gap and return the row
-            row = \
+                row = self.df.set_index("calendar").loc[time]
+            else:
+                # add the time point into the index, then interpolate over the gap and return the row
+                row = \
                 self.df.set_index("calendar").append(pd.DataFrame(index=[time])).sort_index(ascending=True).interpolate(
                     method="time").loc[time]
+
+            if self.doc.c.sim.calendar_to_simtime(time) > self.df.sim_time.max():
+                row["arrived"] = True
+            else:
+                row["arrived"] = False
 
             return row
         else:
             raise ValueError(f"argument time of type {type(time)} not supported.")
+
+    def plot(self):
+        # TODO: add plot function
+        pass
 
 
 class ParticleSwarm:
@@ -233,15 +246,36 @@ class ParticleSwarm:
 
     """
 
-    def __init__(self, doc, default_settings=None):
+    def __init__(self, doc, forward=False, unsteady=False, random_walk=False, default_settings=None):
+
         self.particles = []
         self.doc = doc
 
         if default_settings is None:
             default_settings = {}
+
+        if "tracking_direction" not in default_settings.keys():
+            if forward:
+                default_settings["tracking_direction"] = Enum.PT_DIR_FORWARD
+            else:
+                default_settings["tracking_direction"] = Enum.PT_DIR_BACKWARD
+
+        if "tracking_mode" not in default_settings.keys():
+            if unsteady:
+                default_settings["tracking_mode"] = Enum.PT_MODE_UNSTEADY
+            else:
+                default_settings["tracking_mode"] = Enum.PT_MODE_STEADY
+
+        if "tracking_type" not in default_settings.keys():
+            if random_walk:
+                default_settings["tracking_type"] = Enum.PT_TYPE_RW
+            else:
+                default_settings["tracking_type"] = Enum.PT_TYPE_ADVECTIVE
+
         self.default_settings = default_settings
 
-    def append_particle(self, seed_point, time_start=None, global_cos=True, settings=None, count=1, delay=1):
+    def append_particle(self, seed_point, time_start=None, global_cos=True, settings=None,
+                        count=1, delay=0, progress_indicator=None):
 
         # use default settings of ensemble if no setting is provided
         if settings is None:
@@ -290,17 +324,27 @@ class ParticleSwarm:
                                  global_cos=global_cos, settings=settings,
                                  count=count - 1, delay=delay)
 
+        # update the progress indicator if provided
+        if progress_indicator is not None:
+            progress_indicator.value += 1
+            progress_indicator.description = f"{progress_indicator.value} / {count}"
+
     def particle_tips(self):
         pass
         # TODO
 
-    def particle_position_at_time(self, time):
+    def particle_position_at_time(self, time, remove_arrived=False):
         """
         Returns a (Geo)DataFrame with the location of all particles at a particular time
         """
         df_location = pd.concat([pl.get_location_at_time([time]) for pl in self.particles])
+        df_location.reset_index(inplace=True)
         df_location.index.name = "particle_id"
-        return df_location
+
+        if remove_arrived:
+            return df_location[~df_location.arrived]
+        else:
+            return df_location
 
     def seed_points(self):
         """
