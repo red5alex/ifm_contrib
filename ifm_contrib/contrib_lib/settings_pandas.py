d7327b9b0b1a74c7ff029df89e8f6102d0c62363
RenzA <alexander.renz@bge.de>
2023-06-15 17:41:53 +0200

    added readable_headers option to lookup_table

--- ifm_contrib/contrib_lib/settings_pandas.py
+++ ifm_contrib/contrib_lib/settings_pandas.py
@@ -28,11 +28,13 @@ class SettingsPd:
         # doc.getSpeciesKineticsMonod(0)
         # doc.getSpeciesKineticsUserDefined(0)
 
-    def lookup_table(self, names_as_index=True):
+    def lookup_table(self, names_as_index=True, readable_headers=False):
         """
-        Return the models parameter lookup table as a DataFrame
+        Return the models parameter lookup table as a DataFrame. 
 
         :param names_as_index: Use material names as index (default), otherwise use material ID.
+        :param: readable_headers: Use human readable abbreviations instead of numeric parameter ids as headers. These correspond 
+        to the name of the property in the ifm.Enum object, ommitting the leading `P_` (`ifm.Enum.P_CONDX` -> `CONDX`).
         :return: Parameter Lookup Table
         :rtype: pandas.DataFrame
         """
@@ -58,4 +60,10 @@ class SettingsPd:
         df_lookuptable = df_lookuptable.join(df_properties)
         if names_as_index is True:
             df_lookuptable = df_lookuptable.reset_index().set_index("material_name")
+
+        # rename column headers from parameter ids to readable names
+        if readable_headers:
+            dict_ids = {getattr(ifm.Enum, c): c.replace("P_","") for c in dir(ifm.Enum) if c.startswith("P_")}
+            df_lookuptable.columns = list(df_lookuptable.columns[:1]) + [dict_ids[c] for c in df_lookuptable.columns[1:]]
+            
         return df_lookuptable
